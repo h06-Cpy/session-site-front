8615cb7bd4375be25e7641383f31f072d03662bd
h06-Cpy <hyjun0103@gmail.com>
2022-10-28 18:47:57 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark">
+        <Navbar.Brand href=".">Session</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#잡담방">잡담방(익명)</Nav.Link>
+            <Nav.Link href="#퀴즈">퀴즈</Nav.Link>
+          </Nav>
+          <Nav className="d-flex">
+            <Nav.Link href="#회원가입">회원가입</Nav.Link>
+            <Nav.Link href="#로그인">로그인</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
