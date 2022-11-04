f24a37524297b0a0c476f74265bcbae5cc4cba5d
h06-Cpy <hyjun0103@gmail.com>
2022-11-04 21:26:55 +0900

    making fetchPosts

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,6 +1,31 @@
 import { Button, Col, Container, Row, Table } from "react-bootstrap";
+import axios from "axios";
+import { useState } from "react";
+
+type PostList = {
+  title: string;
+  date: string;
+  commentsNum: number;
+};
 
 function Board() {
+  const [posts, setPosts] = useState<PostList[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPosts = async () => {
+    try {
+      setPosts([]);
+      setLoading(true);
+      const response = await axios.get("http://localhost:8080");
+      setPosts(response.data);
+    } catch (e) {
+      return <div>error occured</div>;
+    }
+    setLoading(false);
+  };
+
+  if (loading) return <div>loading</div>;
+
   return (
     <div>
       <Container className="my-3">
@@ -21,9 +46,19 @@ function Board() {
           <thead>
             <tr>
               <th>제목</th>
+              <th>댓글</th>
               <th>작성일시</th>
             </tr>
           </thead>
+          <tbody>
+            {posts.map((post) => (
+              <tr>
+                <td>{post.title}</td>
+                <td>{post.date}</td>
+                <td>{post.commentsNum}</td>
+              </tr>
+            ))}
+          </tbody>
         </Table>
       </Container>
     </div>
