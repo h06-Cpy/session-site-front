e38f97a88b75b38efd7da4b95b9cb39b1b5f5fc0
h06-Cpy <hyjun0103@gmail.com>
2022-11-27 23:43:24 +0900

    made comment form

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,14 +1,29 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container, Form, Button } from "react-bootstrap";
 import { Post } from "../interfaces";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import DateTimeParser from "../DateTimeParser";
 
 function Detail() {
+  //react hooks must be on top!
   const { id } = useParams();
   const [post, setPost] = useState<Post>();
   const [loading, setLoading] = useState(false);
+  const [comment, setComment] = useState<String>();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  if (id === undefined) {
+    return (
+      <div>
+        <h2>id error</h2>
+      </div>
+    );
+  }
 
   const fetchPost = async () => {
     try {
@@ -21,20 +36,51 @@ function Detail() {
     }
     setLoading(false);
   };
-  useEffect(() => {
-    fetchPost();
-  }, []);
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(event.currentTarget.value as String);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8080/comment", {
+        content: `${comment}`,
+        postId: parseInt(id),
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    navigate(0);
+  };
 
   if (loading) return <div>loading</div>;
   if (post === undefined) return <div>data can't be loaded</div>;
 
   return (
     <div>
-      <Container>
+      <Container className="my-3">
         <h1>{post?.postTitle}</h1>
         <hr></hr>
         <p>{post?.postContent}</p>
         <p>{DateTimeParser(post?.createdDate)}</p>
+        <Form className="my-3" onSubmit={handleSubmit}>
+          <Form.Group controlId="commentContent">
+            <Form.Control
+              required
+              as="textarea"
+              placeholder="내용"
+              rows={7}
+              onChange={handleContentChange}
+            />
+          </Form.Group>
+          <Button className="my-3" variant="dark" type="submit">
+            댓글쓰기
+          </Button>
+        </Form>
         <h3>댓글 {post?.comments.length}개</h3>
         <hr></hr>
         {post?.comments.map((comment) => {
